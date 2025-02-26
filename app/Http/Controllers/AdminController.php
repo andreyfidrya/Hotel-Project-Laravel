5c3d82999a88e9ae487c86d95d966e3c9438b49e
Andrey Fidrya <findhostcoupons@gmail.com>
2025-02-26 19:44:54 +0200

    Adding Login an Register buttons to header

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -20,7 +20,7 @@ class AdminController extends Controller
 
             if($usertype =='user')
             {
-                return view('dashboard');
+                return view('home.index');
             }
 
             else if($usertype =='admin')
