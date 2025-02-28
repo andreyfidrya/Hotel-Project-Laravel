52bd0b99bf48200d86fa2798628b684c307f1f15
Andrey Fidrya <findhostcoupons@gmail.com>
2025-02-28 21:51:27 +0200

    displaying data in Laravel admin panel

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -88,4 +88,14 @@ class AdminController extends Controller
         return redirect()->back();
 
     }
+
+    
+    
+    public function view_room()
+    {
+        $data = Room::all();
+
+        return view('admin.view_room', compact('data'));
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,5 @@ Route::get('/create_room', [AdminController::class, 'create_room']);
 
 
 Route::post('/add_room', [AdminController::class, 'add_room']);
+
+Route::get('/view_room', [AdminController::class, 'view_room']);
