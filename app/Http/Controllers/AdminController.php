188276e27e302feb19d150ca7bd2370cf27dde89
Andrey Fidrya <findhostcoupons@gmail.com>
2025-03-04 22:36:26 +0200

    displaying hotel data on homepage

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -22,7 +22,9 @@ class AdminController extends Controller
 
             if($usertype =='user')
             {
-                return view('home.index');
+                $room = Room::all();
+                
+                return view('home.index', compact('room'));
             }
 
             else if($usertype =='admin')
@@ -44,7 +46,9 @@ class AdminController extends Controller
     public function home()
     {
 
-        return view('home.index');
+        $room = Room::all();
+
+        return view('home.index', compact('room'));
 
     }
 
